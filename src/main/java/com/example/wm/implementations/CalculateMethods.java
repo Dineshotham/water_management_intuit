47c5745f68465631da96047e57a7d4cdd5b95203
Dineshotham Kumar Khambhammettu <dk185173@ncr.com>
2021-11-20 18:25:46 +0530

    Updating more methods

--- src/main/java/com/example/wm/implementations/CalculateMethods.java
+++ src/main/java/com/example/wm/implementations/CalculateMethods.java
@@ -4,20 +4,26 @@ import com.example.wm.interfaces.*;
 public class CalculateMethods implements calculations {
 
     @Override
-    public int initialCalculation(int volume,int r1, int r2 ) {
+    public int initialCostCalculation(int volume,int r1, int r2 ) {
         return 0;
     }
 
     @Override
-    public int slabCalculation(int volume) {
+    public int slabCostCalculation(int volume) {
         return 0;
     }
-
+    @Override
     public int initialVolumeCalculation(int type ){
         return 0;
     }
-
+    @Override
     public int slabVolumeCalculation(int guests){
         return 0;
     }
+
+    @Override
+    public int totalCostCalculation(int intitalCost, int slabCost) {
+        return 0;
+    }
+
 }
--- src/main/java/com/example/wm/interfaces/calculations.java
+++ src/main/java/com/example/wm/interfaces/calculations.java
@@ -1,6 +1,10 @@
 package com.example.wm.interfaces;
-
+//List of all methods that are required for performing the computation
 public interface calculations {
-    int initialCalculation(int volume, int r1, int r2);
-    int slabCalculation(int volume);
+    int initialCostCalculation(int volume, int r1, int r2);
+    int slabCostCalculation(int volume);
+    int initialVolumeCalculation(int type );
+    int slabVolumeCalculation(int guests);
+    int totalCostCalculation(int intitalCost,int slabCost);
+
 }
