77f519911d6575ba5274d213832b0212fab1f847
Dineshotham Kumar Khambhammettu <dk185173@ncr.com>
2021-11-20 18:20:34 +0530

    Sample Inputs added and Caclulation  methods

--- src/main/java/com/example/wm/implementations/CalculateMethods.java
+++ src/main/java/com/example/wm/implementations/CalculateMethods.java
@@ -1,4 +1,23 @@
 package com.example.wm.implementations;
+import com.example.wm.interfaces.*;
 
-public class CalculateMethods {
+public class CalculateMethods implements calculations {
+
+    @Override
+    public int initialCalculation(int volume,int r1, int r2 ) {
+        return 0;
+    }
+
+    @Override
+    public int slabCalculation(int volume) {
+        return 0;
+    }
+
+    public int initialVolumeCalculation(int type ){
+        return 0;
+    }
+
+    public int slabVolumeCalculation(int guests){
+        return 0;
+    }
 }
--- src/main/java/com/example/wm/interfaces/calculations.java
+++ src/main/java/com/example/wm/interfaces/calculations.java
@@ -1,6 +1,6 @@
 package com.example.wm.interfaces;
 
 public interface calculations {
-    int initialCalculation();
-    int slabCalculation();
+    int initialCalculation(int volume, int r1, int r2);
+    int slabCalculation(int volume);
 }
